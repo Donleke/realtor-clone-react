551c19e86065b174a46986e6e65903d88608c27b
leke <lekeyemo@gmail.com>
2024-01-31 09:25:32 +0100

    Complete the oauth functionality

--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -1,8 +1,42 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { useNavigate } from "react-router";
 
 function Oauth() {
+  const navigate = useNavigate();
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check for the user
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timeStamp: serverTimestamp(),
+        });
+      }
+
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  };
+
   return (
     <button
+      type="button"
+      onClick={onGoogleClick}
       className="flex items-center justify-center w-full px-7 py-3 uppercase
      bg-red-700 text-white text-sm font-medium rounded hover:bg-red-800 active:bg-red-900 
      transition duration-150 ease-in-out shadow-md hover:shadow-lg active:shadow-lg"
